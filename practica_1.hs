f1506a8abc1fd54bf0a5b0131121870d6d82a8af
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-03-26 00:04:26 -0300

    Practica 2 terminada menos los ejercicios complementarios

--- practica_1.hs
+++ practica_1.hs
@@ -13,4 +13,5 @@ fib n = fib(n-1)+fib(n-2)
 es_bisiesto x= (mod(mod x 100)4==0 && mod x 100 /= 0) || mod x 400 == 0
 
 --ejercicio 10
-sort3 x y z= min x (min y z)
+between x y z= if x< (max x (max y z )) && x > (min x (min y z )) then x else if y< (max x (max y z )) && y > (min x (min y z )) then y else z
+sort3 x y z= (min x (min y z),between x y z,max x (max y z))
--- practica_2.hs
+++ practica_2.hs
@@ -1,9 +1,70 @@
+--ejercicio1
+  --a
+  --True
+  --b
+  --(5,7)
+  --c
+  ej2_1c x= if x=='h' then 0 else 1
+  --d
+  ej2_1d (x,y)='a'>y || x<1 
+  --e
+  ej2_1e sum =  1+(sum 1) 
+  --f
+  --((True&&),6)
+  --g
+  ej2_1g x = True
+  --h
+  ej2_1h x=x
+
 --ejercicio 4
-first::(A,B)->A
-first (x,y)=x
+  --a first::(a,b)->a
+  first (x,y) = x
+  --b second::(a,b)->b
+  second (x,y) = y
+  --c const::a->b->a
+  consT x y = x
+  --d compose::(b->c)->(a->b)->(a->c)
+  compose f g = (\x -> f (g x))
+  --e apply::(a->b)->a->b
+  apply f x = f x
+  --f subst::(a->b->c)->(a->b)->a->c
+  subst f g x = f x (g x)
+  --g pairFunc::(a->b,b->a)->b->a->(b,a)
+  pairFunc (f1,f2) x y = (f1 (f2 x), f2 (f1 y))
+  
+--ejercicio 6 
+  --a Correcto
+  --b Incorrecto, todo if debe tener else
+  --c  Incorrecto, := es sintacticamente incorrecto
+  --d Incorrecto, error de tipos
+  --e Correcta
+  --f incorrecta, las funciones de mayor, menor, etc son binarias
 
-const:: A->B->A
-const x y =x
+--ejercicio 9
+  --a undefined
+  --b 
+  ej2_9b x=if (x+1)<0 then undefined else undefined
+  --c
+  ej2_9c x=undefined
+  --d
 
-compose::A->T->T
-compose f g= (\x-> f(g x))
+--ejercicio 10
+  --La funcion tom x=x x no es correcta porque al definir el tivo vemos que entra en un loop recursivo sin fin
+
+--ejercicio 11
+  --a
+  smaller::Int->Int->Int->Int
+  smaller = \x->(\y->(\z->if (x<y) && (x<z) then x else if (y<x) && (y<z) then y else z))
+  --b
+  second_2:: a->b->b
+  second_2 = \x->(\x->x)
+  --c
+  andThen::Bool->Bool->Bool
+  andThen= \x->(\z->if x then z else x)
+
+--ejercicio 12
+  --a
+  iff x y | x = not y
+          | not x = y
+  --b
+  alpha x y = y
