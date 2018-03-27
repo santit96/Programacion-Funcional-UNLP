1b531bd4f8124c20e66998398e9d2c6c449e4d69
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-03-27 15:31:14 -0300

    se sigue la pract 3

--- practica_3.hs
+++ practica_3.hs
@@ -16,3 +16,25 @@
     ej3_1a6 x y z= x z y
 --ejercicio2
   --a funo está currificada
+
+--ejercicio5
+  --a
+    twice = \f->(\x->f(f x))
+  --b
+    flip = \f->(\x->(\y->f y x))
+  --c
+    inc2= (+2)
+    inc = \x->x+1
+
+--ejercicio6
+  --a
+  --fix::((a->b)->a->b)->a->b
+    fix f x = f (fix f) x
+  --b
+  --fork (fork,fork) (fork,fork):: 
+  --fork::(a->b,a->c)->a->(b,c)
+    fork (f,g) x = (f x,g x)
+  --c
+  --apply apply apply:: ()
+  --apply::(a->b)->a->b
+    apply f x = f x
