db35acd189bc3cff251a075d250a64624c06dc19
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-06-12 18:32:14 -0300

    terminado ej1 de p7

--- practica_7.hs
+++ practica_7.hs
@@ -18,12 +18,29 @@ remainders y = foldr' (\x n->(mod x y):n) []
 order= foldr' (\(x,y) n->if x<(3*y) then (x,y):n else n) []
 
 --ejercicio 2
+pal x= x == (reverse x)
 hs=foldr' (\x n-> if head x == 'h' then 1+n else n) 0
 avgLength x =div (foldr' ((+) . length ) 0 x) (length x)
+adjacents (x:[])= []
+adjacents (x:xs)= (x,head xs):adjacents xs
 diffAdj (x:[]) =  []
 diffAdj (x:xs) = (x,head xs):diffAdj xs
+remDups [] = []
+remDups (x:xs) = x:remDups (filter (not . (== x )) xs)
+primes::Int->[Int]
+primes 0 = []
+primes n = if (isPrime (abs n)) then n:(primes ((abs n) - 1)) else primes ((abs n)-1)
+isPrime:: Int->Bool
+isPrime n = dividesAny n (n-1)
+dividesAny:: Int->Int->Bool
+dividesAny 1 n = False
+dividesAny n 1 = True
+dividesAny n s = (not (mod n s == 0)) && (dividesAny n (s-1))
 --ejercicio 3
 --f::[a]->[a]
 -- f [a,b,c] <-> foldr (:) [] [a,b,c] <-> : a foldr (:) [] [b,c] <->  
 -- <-> : a (: b foldr (:) [] []) <-> : a (: b (: c foldr (:) [] [] )) <-> 
 -- <-< : a (: b (: c [])) <-> a:b:c:[] <-> [a,b,c]
+
+--ejercicio 4
+
